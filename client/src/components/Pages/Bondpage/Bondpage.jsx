08d764c7a28c715640648b07f5fc6cba56b337bf
Connor Roberts <connoobets@gmail.com>
2022-01-17 12:47:49 -0700

    add bond data on bondpage, change seed to have valid imageUrl, change sign out to work universally

--- client/src/components/Pages/Bondpage/Bondpage.jsx
+++ client/src/components/Pages/Bondpage/Bondpage.jsx
@@ -1,10 +1,47 @@
 import React, { useContext } from 'react';
-
 import { StateContext } from 'context/StateProvider';
 
+import logoImage from 'assets/images/logo.svg';
+import default_avatar from 'assets/images/avatar.png';
+import axios from 'axios';
+
 const Bondpage = () => {
-  const { isAuth, user } = useContext(StateContext);
-  return <div>{isAuth && <div>Bond Page</div>}</div>;
+  const { isAuth, user, bond } = useContext(StateContext);
+ 
+  
+  return (
+  <>
+    <div>{isAuth && <h2>{bond.name}</h2>}</div>
+    {bond ? (
+      <>
+        <img 
+          src={bond.imageUrl ? bond.imageUrl : logoImage }
+          width='300px'
+        />
+        <h4>{bond.bondUsers.length} people in this bond:</h4>
+        <div className="bond-user-list">
+          <ol>{bond.bondUsers.map(user =>
+            <li key={user.id}>
+              <p><strong>{`${user.firstName} ${user.lastName}`}</strong></p>
+              <img 
+                src={user.imageUrl ? user.imageUrl : default_avatar}
+                width='100px'
+                />
+              <p>Email: {user.email}</p> 
+              <hr />
+            </li>
+            )}
+          </ol>
+        </div>
+      </>
+    ) : (
+      <>
+        <div>You are currently not bonded</div>
+        <a href="/">Homepage</a>
+      </>
+    )}
+  </>
+    )
 };
 
 export default Bondpage;
