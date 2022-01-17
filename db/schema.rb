c9e5d69a35ba25cc0147edf4ba533d5ad8729876
Aaron K <tonberryspawn@gmail.com>
2022-01-16 18:57:40 -0800

    package and schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_204410) do
+ActiveRecord::Schema.define(version: 2022_01_16_020559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2022_01_16_204410) do
     t.integer "dosage"
     t.index ["med_group_id"], name: "index_meds_on_med_group_id"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "email"
