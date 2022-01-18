a9d342ec6466bc6374c444555c65327e41504fc0
Aaron K <tonberryspawn@gmail.com>
2022-01-18 13:54:54 -0800

    no message

--- client/src/components/Pages/Graphpage/Graphpage.jsx
+++ client/src/components/Pages/Graphpage/Graphpage.jsx
@@ -6,27 +6,26 @@ import axios from 'axios';
 import './graphpage.scss';
 
 
-
 const data = {
   labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-  datasets:
-  [
-    {
-      name: "Med group 1",
-      chartType: "line",
-      values: [8, 11, 13, 9, 10, 9, 13, 13, 9, 10] // [0,0,0,0,0,0,0,0,0,0] 
-    },
-    {
-      name: "Med group 2",
-      chartType: "line",
-      values: [14, 7, 11, 11, 12, 14, 12, 13, 14, 6] // [0,0,0,0,0,0,0,0,0,0] 
-    },
-    {
-      name: "Med group 3",
-      chartType: "line",
-      values: [16, 7, 14, 13, 8, 12, 15, 9] // [0,0,0,0,0,0,0,0,0,0]
-    }
-  ],
+  datasets: [],
+  // [
+  //   {
+  //     name: "Med group 1",
+  //     chartType: "line",
+  //     values: [8, 11, 13, 9, 10, 9, 13, 13, 9, 10] // [0,0,0,0,0,0,0,0,0,0] 
+  //   },
+  //   {
+  //     name: "Med group 2",
+  //     chartType: "line",
+  //     values: [14, 7, 11, 11, 12, 14, 12, 13, 14, 6] // [0,0,0,0,0,0,0,0,0,0] 
+  //   },
+  //   {
+  //     name: "Med group 3",
+  //     chartType: "line",
+  //     values: [16, 7, 14, 13, 8, 12, 15, 9] // [0,0,0,0,0,0,0,0,0,0]
+  //   }
+  // ],
   
   yMarkers: [
     { label: "Compliance Time", value: 10, options: { labelPos: "left" } }
@@ -36,45 +35,44 @@ const data = {
   ]
 };
 
-
-
 const Graphpage = () => {
   const { userMedGroupArr } = useContext(StateContext);
   const chartRef = useRef();
-  const [historystate, setHistorystate] = useState([]);
-  // const [datastate, setDatastate] = useState(data);
+  const [datastate, setDatastate] = useState(data);
+  // const colorsArr = ['#21ba45', '#7cd6fd', 'purple'];
   // console.log(userMedGroupArr); // => [{id, name, detail, complianceTime, ...}, {}]
   
-  // useEffect(() => {
-  //   // iterate userMedGroupArr > axios req for each
-  //   userMedGroupArr.forEach(medGroupItem => (
-  //     axios
-  //       .get('/med_groups/' + medGroupItem.id)
-  //       .then(resultsArr => {
-  //         const historyArr = resultsArr.data.historyTenDays;
-  //         const timeArr = historyArr.map(obj =>
-  //           Number(obj.created_at.substring(11, 13))
-  //         );
-  //         setHistorystate(prev => [...prev, { name: medGroupItem.name, timeArr: timeArr }]);
-  //       })
-  //     .catch(err => console.log(err.response.data.error))
-  //   ));
+  // send 1 get, update data set 1 time;
+  useEffect(() => {
 
-  //   const timeDataSets = historystate.map(obj => (
-  //     {
-  //       name: obj.name,
-  //       chartType: "line",
-  //       values: obj.timeArr
-  //     }
-  //   ));
-  //   console.log(timeDataSets);
-  //   data.datasets = timeDataSets;
-  //   // setDatastate(prev => ({...prev, datasets: }))
+    const promises = userMedGroupArr.forEach(medGroupItem => (
+      axios
+        .get('/med_groups/' + medGroupItem.id)
+        .then(resultsArr => {
+          const historyArr = resultsArr.data.historyTenDays;
+          const timeNumArr = historyArr.map(obj => Number(obj.created_at.substring(11, 13)));
+          const datasetObj = {
+            name: medGroupItem.name,
+            chartType: "line",
+            values: timeNumArr
+          }
+          // console.log(datasetObj); // => correct output
+          
+          setDatastate(prev => (
+            {...prev, datasets: [...prev.datasets, datasetObj]} // {}
+          ));
+        })
+        .catch(err => console.log(err.response.data.error))
+    ));
 
-  // }, []);
+  }, [userMedGroupArr, setDatastate]);
   // console.log(JSON.stringify(historystate)); 
 
-
+  // return (
+  //   <pre>
+  //     {JSON.stringify(datastate, null, 2)}
+  //   </pre>
+  // )
 
   const exportChart = () => {
     if (chartRef && chartRef.current) {
@@ -84,27 +82,27 @@ const Graphpage = () => {
 
   return (
     <div className='graphpage'>Graph Page
-      <ReactFrappeChart
-        ref={chartRef}
-        title={"Compliance Data Chart"}
-        type='axis-mixed' // 'axis-mixed', 'bar', 'line', 'scatter', 'pie', 'percentage'
-        colors={['#21ba45', '#7cd6fd'/* , 'purple' */]}
-        axisOptions={{
-          xAxisMode: "tick",
-          yAxisMode: "tick",
-          xIsSeries: 1
-        }}
-        height={350}
-        data={data}
-        tooltipOptions={{
-          formatTooltipX: (d) => ("day " + d).toUpperCase(),
-          formatTooltipY: (d) => d + "00 hours"
-        }}
-      />
-    <button onClick={exportChart} type="button">
-      Export
-    </button>
-    </div>
+        <ReactFrappeChart
+          ref={chartRef}
+          title={"Compliance Data Chart"}
+          type='axis-mixed' // 'axis-mixed', 'bar', 'line', 'scatter', 'pie', 'percentage'
+          colors={['#21ba45', '#7cd6fd'/*, 'purple' */]}
+          axisOptions={{
+            xAxisMode: "tick",
+            yAxisMode: "tick",
+            xIsSeries: 1
+          }}
+          height={350}
+          data={data}
+          tooltipOptions={{
+            formatTooltipX: (d) => ("day " + d).toUpperCase(),
+            formatTooltipY: (d) => d + "00 hours"
+          }}
+        />
+        <button onClick={exportChart} type="button">
+          Export
+        </button>
+  </div>
   );
 };
 
