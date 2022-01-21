2fb43c6d247f1da295385224c62b3d43941aab0e
Aaron K <tonberryspawn@gmail.com>
2022-01-21 01:25:18 -0800

    cleaning up code

--- client/src/components/Pages/Graphpage/Graphpage.jsx
+++ client/src/components/Pages/Graphpage/Graphpage.jsx
@@ -3,49 +3,11 @@ import { StateContext } from '../../../context/StateProvider';
 import axios from 'axios';
 import { AreaChart, Area, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+// Chart data template
 // const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
+//   { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+//   { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+//   ...
 // ];
 
 // Create dynamic Arr of past 10 days => ['2022-01-10', '2022-01-11', ...]
@@ -106,9 +68,7 @@ const Graphpage = () => {
         }
         console.log(chartDataArr); // => correct output check
 
-        setDatastate(prev => (
-          [...chartDataArr]
-        ));
+        setDatastate(prev => ([...chartDataArr]));
       })
       .catch(err => console.log(err.response.data.error));
   }, [userMedGroupArr]);
