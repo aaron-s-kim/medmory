6f03dcbd4f6bb57131d6f485380922a1fb4f92f5
Aaron K <tonberryspawn@gmail.com>
2022-01-21 00:09:17 -0800

    finished adding dynamic data to graph

--- client/src/components/Pages/Graphpage/Graphpagev2.jsx
+++ client/src/components/Pages/Graphpage/Graphpagev2.jsx
@@ -141,7 +141,7 @@ const Graphpagev2 = () => {
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <AreaChart width={730} height={250} data={data}
+      <AreaChart width={730} height={250} data={datastate}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -159,7 +159,7 @@ const Graphpagev2 = () => {
         <Tooltip />
         <Area type="monotone" dataKey="Vitamins" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
         <Area type="monotone" dataKey="The Drugs" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-        <Area type="monotone" dataKey="Hypertension Medications" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+        <Area type="monotone" dataKey="Hypertension Medications" stroke="#83ca9d" fillOpacity={1} fill="url(#colorPv)" />
       </AreaChart>
     </ResponsiveContainer>
   )
